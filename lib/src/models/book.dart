43bae5b436e72f14ad6c72e5e54a513b3fc78260
devrnt <devrient.jonas@gmail.com>
2019-07-11 15:51:06 +0200

    Add book model

---
+++ lib/src/models/book.dart
@@ -0,0 +1,32 @@
+class Book {
+  final String title;
+  final String author;
+  final String description;
+  final String coverUrl;
+  final String category;
+
+  Book(this.title, this.author, this.description, this.coverUrl, this.category);
+}
+
+final books = [
+  Book(
+    'The Doe in the Forest',
+    'Laurel Toven',
+    'In this book, we have a geographical historical and cultural region in the wester, part of the country of Belgium',
+    'https://about.canva.com/wp-content/uploads/sites/3/2015/01/children_bookcover.png',
+    'Children',
+  ),
+  Book(
+      'Norse Mythology',
+      'Neil Gaiman',
+      'The immortal #1 New York Times bestseller',
+      'https://pro2-bar-s3-cdn-cf3.myportfolio.com/560d16623f9c2df9615744dfab551b3d/e50c016f-b6a8-4666-8fb8-fe6bd5fd9fec_rw_1920.jpeg?h=dc627898fc5eac88aa791fb2b124ecbd',
+      'Religion'),
+  Book(
+    'The Sun, the Moon, the Stars',
+    'Junot Diaz',
+    'Winner of the Pulitzer Prize',
+    'https://pro2-bar-s3-cdn-cf4.myportfolio.com/2e52194b029a65d876d57172b412d63e/5a0ce3f3-5f59-4098-8d45-93a73cf2800c_rw_1200.png?h=40a11543bd7bdc2d8e956150e3a5af2c',
+    'Drama',
+  ),
+];
