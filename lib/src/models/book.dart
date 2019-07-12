b66e666d899d2bafc717a3dfbf69a51b7c4bea75
devrnt <devrient.jonas@gmail.com>
2019-07-12 09:25:00 +0200

    Add rating property to book model

--- lib/src/models/book.dart
+++ lib/src/models/book.dart
@@ -4,8 +4,10 @@ class Book {
   final String description;
   final String coverUrl;
   final String category;
+  final num rating;
 
-  Book(this.title, this.author, this.description, this.coverUrl, this.category);
+  Book(this.title, this.author, this.description, this.coverUrl, this.category,
+      this.rating);
 }
 
 final initialBooks = [
@@ -15,19 +17,23 @@ final initialBooks = [
     'In this book, we have a geographical historical and cultural region in the wester, part of the country of Belgium',
     'https://about.canva.com/wp-content/uploads/sites/3/2015/01/children_bookcover.png',
     'Children',
+    9.0,
   ),
   Book(
-      'Norse Mythology',
-      'Neil Gaiman',
-      'The immortal #1 New York Times bestseller',
-      'https://pro2-bar-s3-cdn-cf3.myportfolio.com/560d16623f9c2df9615744dfab551b3d/e50c016f-b6a8-4666-8fb8-fe6bd5fd9fec_rw_1920.jpeg?h=dc627898fc5eac88aa791fb2b124ecbd',
-      'Religion'),
+    'Norse Mythology',
+    'Neil Gaiman',
+    'The immortal #1 New York Times bestseller',
+    'https://pro2-bar-s3-cdn-cf3.myportfolio.com/560d16623f9c2df9615744dfab551b3d/e50c016f-b6a8-4666-8fb8-fe6bd5fd9fec_rw_1920.jpeg?h=dc627898fc5eac88aa791fb2b124ecbd',
+    'Religion',
+    7.0,
+  ),
   Book(
     'The Sun, the Moon, the Stars',
     'Junot Diaz',
     'Winner of the Pulitzer Prize',
     'https://pro2-bar-s3-cdn-cf4.myportfolio.com/2e52194b029a65d876d57172b412d63e/5a0ce3f3-5f59-4098-8d45-93a73cf2800c_rw_1200.png?h=40a11543bd7bdc2d8e956150e3a5af2c',
     'Drama',
+    6.7,
   ),
 ];
 
@@ -37,4 +43,5 @@ final bookToAdd = Book(
   'In an age with kings and kings',
   'https://www.harrypotterfanzone.com/article/wp-content/uploads/2018/11/jonny-duddle-goblet-of-fire-cover.png',
   'Fantasy',
+  9.2,
 );
