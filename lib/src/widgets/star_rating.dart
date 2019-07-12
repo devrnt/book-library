769d33ef31582a257c149031721618e3b08ae501
devrnt <devrient.jonas@gmail.com>
2019-07-12 12:17:12 +0200

    Add star rating widget

---
+++ lib/src/widgets/star_rating.dart
@@ -0,0 +1,44 @@
+import 'package:book_library/src/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
+
+class StarRating extends StatelessWidget {
+  final num size;
+  final int starCount;
+  final num rating;
+
+  StarRating({
+    @required this.starCount,
+    @required this.rating,
+    this.size = 20.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        SmoothStarRating(
+          size: size,
+          // FIXME: Is this an issue in the SmoothStarRating package
+          allowHalfRating: false,
+          starCount: 5,
+          rating: rating,
+          color: kYellow,
+          borderColor: kYellow.withOpacity(0.5),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            '$rating',
+            style: TextStyle(
+              color: kYellow,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
