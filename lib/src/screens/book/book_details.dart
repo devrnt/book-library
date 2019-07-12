11167582e28e0fb4bdba849a376e6e765b60e6b3
devrnt <devrient.jonas@gmail.com>
2019-07-12 12:18:41 +0200

    Update book details screen

--- lib/src/screens/book/book_details.dart
+++ lib/src/screens/book/book_details.dart
@@ -1,7 +1,9 @@
-import 'package:book_library/src/models/book.dart';
-import 'package:book_library/src/models/notifiers/book_notifier.dart';
+import 'package:book_library/src/widgets/book_cover.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:book_library/src/models/book.dart';
+import 'package:book_library/src/models/notifiers/book_notifier.dart';
+import 'package:book_library/src/widgets/star_rating.dart';
 
 class BookDetails extends StatelessWidget {
   final Book _book;
@@ -13,48 +15,84 @@ class BookDetails extends StatelessWidget {
     var bookNotifier = Provider.of<BookNotifier>(context);
 
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(20.0),
-        color: Colors.green,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Flexible(
-              flex: 6,
-              child: Row(
-                children: [
-                  Flexible(
-                    flex: 5,
-                    child: FadeInImage.assetNetwork(
-                      placeholder: 'assets/images/book-cover-placeholder.png',
-                      image: _book.coverUrl,
-                      fit: BoxFit.cover,
-                      fadeInDuration: const Duration(milliseconds: 350),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 5,
-                    child: Container(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('${_book.title}'),
-                          Text('By ${_book.author}'),
-                          Text('By ${_book.category}'),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+      appBar: AppBar(
+        title: Text('Details'),
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          margin: const EdgeInsets.symmetric(horizontal: 18.0),
+          // color: Colors.green,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: BookCover(
+                  url: _book.coverUrl,
+                  boxFit: BoxFit.fitHeight,
+                  height: 325,
+                ),
               ),
-            ),
-            Flexible(
-              flex: 4,
-              child: Text(_book.description),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 32.0, 0.0, 4.0),
+                child: Text(
+                  '${_book.title}',
+                  style: Theme.of(context).textTheme.title,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'By ',
+                        style: Theme.of(context).textTheme.caption.copyWith(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                      ),
+                      TextSpan(
+                        text: '${_book.author}',
+                        style: Theme.of(context).textTheme.caption.copyWith(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w700,
+                            ),
+                      ),
+                      TextSpan(
+                        text: ' in ',
+                        style: Theme.of(context).textTheme.caption.copyWith(
+                            fontSize: 16.0, fontWeight: FontWeight.w400),
+                      ),
+                      TextSpan(
+                        text: '${_book.category}',
+                        style: Theme.of(context).textTheme.caption.copyWith(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w700,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              StarRating(
+                starCount: 5,
+                rating: (_book.rating / 2).toDouble(),
+              ),
+              Divider(
+                color: Colors.grey.withOpacity(0.5),
+                height: 38.0,
+              ),
+              Text(
+                '${_book.description}',
+                style: TextStyle(
+                    color: Colors.grey.withOpacity(0.8),
+                    fontFamily: 'Nunito',
+                    fontSize: 16.0),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
