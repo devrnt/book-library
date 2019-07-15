80f7c44b523c0446c055e88234cd2ed391a6f4fc
devrnt <devrient.jonas@gmail.com>
2019-07-15 21:19:05 +0200

    Add adaptive home screen

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,23 +1,61 @@
-import 'package:book_library/src/models/notifiers/theme_notifier.dart';
+import 'package:book_library/src/style.dart';
 import 'package:flutter/material.dart';
-import 'package:book_library/src/screens/book/book_add.dart';
-import 'package:book_library/src/widgets/book_list.dart';
 import 'package:provider/provider.dart';
+
+import 'package:book_library/src/models/notifiers/book_notifier.dart';
+import 'package:book_library/src/models/notifiers/theme_notifier.dart';
+import 'package:book_library/src/screens/book/book_add.dart';
+import 'package:book_library/src/screens/book/book_details_tablet.dart';
+import 'package:book_library/src/widgets/book_list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var themeNotifier = Provider.of<ThemeNotifier>(context);
+    var bookNotifier = Provider.of<BookNotifier>(context);
+    // FIXME: Want to set wideScreen here but it can't be null
+    // Don't know why it is not possible to initialize it here
 
     return Scaffold(
       appBar: _buildAppBar(context, themeNotifier),
       body: Container(
-        child: BookList(),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final wideScreen = constraints.maxWidth > wideLayoutThreshold;
+            if (wideScreen) {
+              return Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 4,
+                    child: BookList(),
+                  ),
+                  Flexible(
+                    flex: 6,
+                    child: BookDetailsTablet(
+                        bookNotifier.books[bookNotifier.selectedIndex]),
+                  ),
+                ],
+              );
+            } else {
+              return BookList();
+            }
+          },
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) => BookAdd()));
+      floatingActionButton: LayoutBuilder(
+        builder: (context, constraints) {
+          final wideScreen = constraints.maxWidth > wideLayoutThreshold;
+          if (wideScreen) {
+            return Container();
+          } else {
+            return FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => BookAdd()));
+              },
+            );
+          }
         },
       ),
     );
@@ -28,14 +66,11 @@ class HomeScreen extends StatelessWidget {
       title: Text('Book Library'),
       actions: [
         IconButton(
-          icon: themeNotifier.darkModeEnabled
-              ? Icon(Icons.brightness_7)
-              : Icon(Icons.brightness_2),
-          color: Theme.of(context).iconTheme.color,
-          onPressed: () {
-            themeNotifier.toggleTheme();
-          },
-        )
+            icon: themeNotifier.darkModeEnabled
+                ? Icon(Icons.brightness_7)
+                : Icon(Icons.brightness_2),
+            color: Theme.of(context).iconTheme.color,
+            onPressed: () => themeNotifier.toggleTheme())
       ],
     );
   }
---
+++ lib/src/style.dart
@@ -0,0 +1,2 @@
+/// Once the logic screen pixel width exceeds this number, show the wide layout.
+const double wideLayoutThreshold = 800;
